56a4d34c689c5d341f1a2e875fdea2f4adaefa68
ken-mu <gas.010bl@gmail.com>
2018-09-24 19:44:37 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu
+
+VERSION=2_8_2
+
+# remove artifact if exist
+if [ -e build ]; then
+  rm -r build
+fi
+mkdir -p build/NOOBS_v${VERSION}
+
+# download NOOBS
+wget http://downloads.raspberrypi.org/NOOBS/images/NOOBS-2018-06-29/NOOBS_v${VERSION}.zip
+unzip NOOBS_v${VERSION}.zip -d build/NOOBS_v${VERSION}
+
+# append "silentinstall"
+cd build/NOOBS_v${VERSION}
+sed -i 's|$| silentinstall|' recovery.cmdline
+
+# Remove other OS
+cd os
+ls|egrep -v '^Raspbian$'|xargs rm -r
+
+# enable SSH
+sed -i -e '/^umount \/tmp\/1/i\touch /tmp/1/ssh' Raspbian/partition_setup.sh
+
+cd ..
+zip -r ../NOOBS_SETUP_v${VERSION}.zip .
