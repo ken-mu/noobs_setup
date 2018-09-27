b6539878c149145cf0d8c7f6157ba37735f7a8e4
ken-mu <gas.010bl@gmail.com>
2018-09-27 19:31:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,5 +28,7 @@ ls|egrep -v '^Raspbian$'|xargs rm -r
 # enable SSH
 sed -i -e '/^umount \/tmp\/1/i\touch /tmp/1/ssh' Raspbian/partition_setup.sh
 
-cd ..
+cd ../
 zip -r ../../artifacts/NOOBS_SETUP_v${VERSION}.zip .
+cd ../../
+ll && pwd
