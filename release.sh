f7fa2daf1c26ba077564ba371ee48f29e5f452c0
ken-mu <gas.010bl@gmail.com>
2018-09-29 15:45:41 +0900

    Update release.sh

--- release.sh
+++ release.sh
@@ -10,9 +10,8 @@ fi
 wget https://github.com/tcnksm/ghr/releases/download/v0.12.0/ghr_v0.12.0_linux_amd64.tar.gz
 tar xvfz ghr_v0.12.0_linux_amd64.tar.gz
 
-echo $1
-echo $CIRCLE_PROJECT_USERNAME
-echo $CIRCLE_PROJECT_REPONAME
+echo TAG: $1
+echo 'files in artifacts:'
 ls -l artifacts
 
-./ghr_v0.12.0_linux_amd64/ghr -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME -replace $1 ./artifacts/
+./ghr_v0.12.0_linux_amd64/ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME -replace $1 ./artifacts/
