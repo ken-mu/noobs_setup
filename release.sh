15edce766a56692073ea604c16bcf83ce2721995
ken-mu <gas.010bl@gmail.com>
2018-09-29 14:32:30 +0900

    Update release.sh

--- release.sh
+++ release.sh
@@ -12,5 +12,7 @@ tar xvfz ghr_v0.12.0_linux_amd64.tar.gz
 
 echo $1
 echo $CIRCLE_PROJECT_USERNAME
+ls -l artifacts
 
-./ghr_v0.12.0_linux_amd64/ghr -u $CIRCLE_PROJECT_USERNAME --delete $1 ./artifacts
+./ghr_v0.12.0_linux_amd64/ghr
+./ghr_v0.12.0_linux_amd64/ghr -u $CIRCLE_PROJECT_USERNAME -delete $1 ./artifacts/
